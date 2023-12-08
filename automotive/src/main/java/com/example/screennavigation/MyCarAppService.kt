06552d63d46c59a31b211ae91a8c6caadb39fda2
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-12-08 13:55:42 +0100

    setUp

---
+++ automotive/src/main/java/com/example/screennavigation/MyCarAppService.kt
@@ -0,0 +1,31 @@
+package com.example.screennavigation
+
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import androidx.car.app.CarAppService
+import androidx.car.app.Screen
+import androidx.car.app.Session
+import androidx.car.app.validation.HostValidator
+
+class MyCarAppService : CarAppService() {
+
+    override fun createHostValidator(): HostValidator =
+        if (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0) {
+            HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
+        } else {
+            HostValidator.Builder(applicationContext)
+                .addAllowedHosts(androidx.car.app.R.array.hosts_allowlist_sample)
+                .build()
+        }
+    override fun onCreateSession(): Session {
+        return HelloWorldSession()
+    }
+}
+
+class HelloWorldSession : Session() {
+
+    override fun onCreateScreen(intent: Intent): Screen {
+        return HelloWorldScreen(carContext)
+    }
+}
+
